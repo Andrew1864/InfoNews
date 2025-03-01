68fdb136c8638cb0ca873848a98f282c1267bcf8
Andrew <huli-gan_94@mail.ru>
2025-03-01 14:17:48 +0300

    Добавил компонент ActiveLink

--- src/app/components/ui/Header/Header.tsx
+++ src/app/components/ui/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ActiveLink from "../ActiveLink/ActiveLink";
 
 const Header = () => {
 
@@ -11,14 +12,14 @@ const Header = () => {
                     </a>
                     <div className="hidden justify-around items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <Link className="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-white lg:p-0 dark:text-white" aria-current="page"
+                            <ActiveLink className="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-white lg:p-0 dark:text-white" aria-current="page"
                             href="/">
                                   Home
-                            </Link>
-                            <Link className="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-white lg:p-0 dark:text-white" aria-current="page"
+                            </ActiveLink>
+                            <ActiveLink className="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-white lg:p-0 dark:text-white" aria-current="page"
                             href="/trading">
                                   Трейдинг
-                            </Link>
+                            </ActiveLink>
                             <li>
                                 <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Биржа</a>
                             </li>
@@ -28,10 +29,10 @@ const Header = () => {
                             <li>
                                 <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Игро-новости</a>
                             </li>
-                            <Link className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                            <ActiveLink className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                             href='/weather'>
                                 Погода
-                            </Link>
+                            </ActiveLink>
                         </ul>
                     </div>
                 </div>
