ff011157e947336028fcb2cace148e4aa6712889
Andrew <huli-gan_94@mail.ru>
2025-03-12 15:59:48 +0300

    Добавил страницу news и компонент карточки

--- src/app/components/ui/Header/Header.tsx
+++ src/app/components/ui/Header/Header.tsx
@@ -26,10 +26,10 @@ const Header = () => {
                                 <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Биржа</a>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Новости</a>
+                                <a href="/news" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Новости</a>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Игро-новости</a>
+                                <ActiveLink href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Игро-новости</ActiveLink>
                             </li>
                             <ActiveLink className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                             href='/weather'>
---
+++ src/app/news/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Card from "@/components/ui/Card/Card";
+
+export default function News() {
+  return (
+    <section id="news">
+      <div className="container mt-3 flex items-center flex-col content-center justify-center max-w-7xl mx-auto px-2 mb-3">
+        <div className="">
+          <Card />
+        </div>
+      </div>
+    </section>
+  );
+}
