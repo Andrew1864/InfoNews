d403e9a6a548a04d782895354e5c7376b0b3d33e
Andrew <huli-gan_94@mail.ru>
2025-03-20 18:00:57 +0300

    Сделал страницу с криптовалютой

--- src/app/exchange/page.tsx
+++ src/app/exchange/page.tsx
@@ -1,16 +1,73 @@
+"use client";
 
+import { useEffect, useState } from "react";
+import Table from "@/components/ui/Table/Table";
+import {
+  fetchCryptos,
+  CryptoData,
+} from "@/services/Api/Cryptocurrency/CryptocurrencyApi";
 
-export default function Exchange (){
-return (
+interface CryptoProps {
+  id: string;
+  name: string;
+  symbol: string;
+  price_usd: string;
+  rank: number;
+}
+
+export default function Exchange() {
+  const [cryptos, setCryptos] = useState<CryptoData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const data = await fetchCryptos(10);
+        setCryptos(data);
+      } catch (err) {
+        setError("Ошибка загрузки данных");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadData();
+  }, []);
+
+  return (
     <>
-  <section className="flex flex-wrap justify-center gap-4">
-      <div className="md:w-[60%]">
-       <h1>Exchange1</h1>
-      </div>
-      <div className="md:w-[20%]">
-      <h1>Exchange2</h1>
-      </div>
-    </section>
+      <section className="flex flex-wrap justify-center gap-4">
+        <div className="md:w-[60%] mb-3 mt-3 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-xl shadow-lg p-4">
+          <h1 className="text-lg text-center font-semibold text-gray-800 dark:text-white mb-4">
+            Таблица криптовалют
+          </h1>
+
+          {loading && (
+            <p className="text-gray-600 dark:text-gray-400">Загрузка...</p>
+          )}
+          {error && <p className="text-red-500">{error}</p>}
+
+          {!loading && !error && (
+            <Table
+              headers={[
+                { key: "rank", title: "Ранг" },
+                { key: "name", title: "Название" },
+                { key: "symbol", title: "Символ" },
+                { key: "price_usd", title: "Цена (USD)" },
+              ]}
+              data={cryptos.map((crypto) => ({
+                rank: crypto.rank,
+                name: crypto.name,
+                symbol: crypto.symbol,
+                price_usd: `$${crypto.price_usd}`,
+              }))}
+            />
+          )}
+        </div>
+        <div className="md:w-[20%]">
+          <h1>Exchange2</h1>
+        </div>
+      </section>
     </>
-)
-};
+  );
+}
--- src/app/services/Api/Cryptocurrency/CryptocurrencyApi.ts
+++ src/app/services/Api/Cryptocurrency/CryptocurrencyApi.ts
@@ -1,21 +1,23 @@
-export interface CryptoData  {
-id: string;
-name: string;
-symbol: string;
-price_usd: string;
-rank: number;
-};
+export interface CryptoData {
+  id: string;
+  name: string;
+  symbol: string;
+  price_usd: string;
+  rank: number;
+}
 
-export const fetchCryptos = async (limit: number = 10): Promise<CryptoData[]> => {
-    try {
-        const response = await fetch("https://api.coinlore.net/api/tickers/");
-        if (!response.ok) {
-            throw new Error("Ошибка при получении данных");
-        };
-        const data = await response.json();
-        return data.data.slice(0, limit);
-    } catch (error) {
-        console.error("Ошибка", error)
-        return [];
+export const fetchCryptos = async (
+  limit: number = 10
+): Promise<CryptoData[]> => {
+  try {
+    const response = await fetch("https://api.coinlore.net/api/tickers/");
+    if (!response.ok) {
+      throw new Error("Ошибка при получении данных");
     }
+    const data = await response.json();
+    return data.data.slice(0, limit);
+  } catch (error) {
+    console.error("Ошибка", error);
+    return [];
+  }
 };
